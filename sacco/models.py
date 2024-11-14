b94a32e6533df2c8eeb2279bf9f103ccca71dbfa
ianmukuna97 <ianmukuna97@gmail.com>
2024-11-14 04:21:39 +0300

    sql insertion

--- sacco/models.py
+++ sacco/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    dob = models.DateField()
+    gender = models.CharField(max_length=10)
+    weight = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.gender}"
+
+    class Meta:
+        db_table = 'customers'
+
+class Deposit(models.Model):
+    amount = models.IntegerField()
+    status = models.BooleanField(default=False)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.customer.first_name} - {self.amount}"
+
+    class Meta:
+        db_table = 'deposits'
+
+
+
+
+# run the migrations
+# python manage.py makemigrations
+# python manage.py migrate
--- sacco/views.py
+++ sacco/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from sacco.models import Customer, Deposit
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
+def test(request):
+    # Uncomment to create new customers
+    # c1 = Customer(first_name='Saida', last_name='Ali',
+    #               email='saida@x.com', dob='2000-11-28', gender='Female', weight=62)
+    # c1.save()
+    #
+    # c2 = Customer(first_name='Jake', last_name='Juma',
+    #               email='juma@x.com', dob='1999-11-28', gender='Male', weight=62)
+    # c2.save()
+
+    customer_count = Customer.objects.count()
+
+    try:
+        c1 = Customer.objects.get(id=1)  # Fetch customer with id=1
+        d1 = Deposit(amount=1000, status=True, customer=c1)
+        d1.save()
+    except ObjectDoesNotExist:
+        return HttpResponse("Customer with id=1 does not exist.")
+
+    deposit_count = Deposit.objects.count()
+    return HttpResponse(f"Ok, Done. We have {customer_count} customers and {deposit_count} deposits.")
